a26d26ee5abbeaee8de47674375f87906baa1a15
Russellier <russellvap13@gmail.com>
2024-08-06 14:30:36 +0800

    feat: Create board for game

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hi');
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  let cell = 0;
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(cell);
+      cell++;
+    }
+  }
+
+  console.log(board);
+}
+
+Gameboard();
